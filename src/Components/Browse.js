9d0cb538cf8df0dc82f380f6766de8cae467a2b1
JatinSehra <jatinsehra2003@gmail.com>
2024-09-17 21:48:03 +0530

    Added new features and fixed bugs

--- src/Components/Browse.js
+++ src/Components/Browse.js
@@ -1,13 +1,16 @@
 
 import useNowPlayingMovies from "../Hooks/useNowPlayingMovies";
 import Header from "./Header";
-
+import MainContainer from './MainContainer'
+import SecoundaryContainer from './SecoundaryContainer'
 const Browse=()=>{
     
     useNowPlayingMovies();
     return(
     <div>
     <Header/>
+    <MainContainer/>
+    <SecoundaryContainer/>
     </div>
     );
 };
